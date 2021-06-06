670d818c95f8f09d74714b388abb6d56feac2f37
= <mahmudur.rahman.csedu@gmail.com>
2021-06-06 08:57:16 +0600

    jquery cdn added and smooth scrolling,opacity of navbar added

---
+++ EDGELEDGER_WEBSITE/js/main.js
@@ -0,0 +1,28 @@
+
+// sticky navbar opacity
+window.addEventListener('scroll',function(){
+    if(window.scrollY > 150){
+        document.querySelector('.navbar').style.opacity = 0.9;
+    }else{
+        document.querySelector('.navbar').style.opacity = 1;
+    }
+});
+
+
+// smooth scrolling
+$('.navbar a, .btn').on('click',function(event){
+    if(this.hash !== ""){
+        console.log(this.hash);
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top -100
+            },
+            800
+        ); 
+    }
+});
+
+
